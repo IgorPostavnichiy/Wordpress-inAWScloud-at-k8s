e6af935e0a4136cdfdeba7b0f0d22ea5f42d1104
IgorPostavnichiy <igorpostavnichiy@gmail.com>
2023-05-09 20:05:16 +0300

    changes in deployfile9

--- 0-aws-provider.tf
+++ 0-aws-provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   access_key = "${secrets.AWS_ACCESS_KEY_ID}"
   secret_key = "${secrets.AWS_SECRET_ACCESS_KEY}" 
-  region     = "${secrets.AWS_REGION }"
+  region = "eu-north-1"
 }
 
 terraform {
