91b2038bd353f5681021a2ee32bb4b5ee4bc7c9a
IgorPostavnichiy <igorpostavnichiy@gmail.com>
2023-05-15 20:21:52 +0300

    changes in deployfile24

--- 3-s3-bucket.tf
+++ 3-s3-bucket.tf
@@ -3,49 +3,51 @@ resource "aws_s3_bucket" "igor-bucket-test1" {
   bucket = "eks-bucket-testmysprout1"
 }
 
-# # Create an IAM policy that allows access to the S3 bucket
-# resource "aws_iam_policy" "s3_policy" {
-#   name = "my-eks-s3-policy"
+# Create an IAM policy that allows access to the S3 bucket
+resource "aws_iam_policy" "s3_policy" {
+  name = "my-eks-s3-policy"
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = [
-#           "s3:Get*",
-#           "s3:List*",
-#           "s3:Put*",
-#           "s3:Delete*"
-#         ]
-#         Effect   = "Allow"
-#         Resource = "${aws_s3_bucket.igor-bucket-test1.arn}/*"
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:Get*",
+          "s3:List*",
+          "s3:Put*",
+          "s3:Delete*"
+        ]
+        Effect   = ["Allow", "kms:CreateKey" ]
+        Resource = "${aws_s3_bucket.igor-bucket-test1.arn}/*"
+      }
+    ]
+  })
+}
 
-# #Create an IAM role that can assume the EKS worker node role
-# resource "aws_iam_role" "worker_role" {
-#   name = "my-eks-worker-role"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#       }
-#     ]
-#   })
-# }
+#Create an IAM role that can assume the EKS worker node role
+resource "aws_iam_role" "worker_role" {
+  name = "my-eks-worker-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = ["Allow", "kms:CreateKey" ]
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
 
-#Attach the S3 policy to the IAM role
-# resource "aws_iam_role_policy_attachment" "worker_role_policy" {
-#   policy_arn = aws_iam_policy.s3_policy.arn
-#   role       = aws_iam_role.worker_role.name
-# }
+Attach the S3 policy to the IAM role
+resource "aws_iam_role_policy_attachment" "worker_role_policy" {
+  policy_arn = aws_iam_policy.s3_policy.arn
+  role       = aws_iam_role.worker_role.name
+}
+
+"kms:CreateKey"
 
 
 # Output the S3 bucket name and ARN
