ffd491c14327d2db9303d3163bd836399d469025
IgorPostavnichiy <igorpostavnichiy@gmail.com>
2023-05-16 08:57:50 +0300

    changes in deployfile26

--- 3-s3-bucket.tf
+++ 3-s3-bucket.tf
@@ -1,57 +1,13 @@
-# Create an S3 bucket
-resource "aws_s3_bucket" "igor-bucket-test1" {
-  bucket = "eks-bucket-testmysprout1"
-}
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
 
-# Create an IAM policy that allows access to the S3 bucket
-resource "aws_iam_policy" "s3_policy" {
-  name = "my-eks-s3-policy"
+  bucket = "s3-bucket-test-itsprout"
+  acl    = "private"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "s3:Get*",
-          "s3:List*",
-          "s3:Put*",
-          "s3:Delete*"
-        ]
-        Effect   = ["Allow", "kms:CreateKey" ]
-        Resource = "${aws_s3_bucket.igor-bucket-test1.arn}/*"
-      }
-    ]
-  })
-}
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
 
-#Create an IAM role that can assume the EKS worker node role
-resource "aws_iam_role" "worker_role" {
-  name = "my-eks-worker-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = ["Allow", "kms:CreateKey" ]
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-#Attach the S3 policy to the IAM role
-resource "aws_iam_role_policy_attachment" "worker_role_policy" {
-  policy_arn = aws_iam_policy.s3_policy.arn
-  role       = aws_iam_role.worker_role.name
-}
-
-# Output the S3 bucket name and ARN
-output "s3_bucket_name" {
-  value = aws_s3_bucket.igor-bucket-test1.bucket
-}
-
-output "s3_bucket_arn" {
-  value = aws_s3_bucket.igor-bucket-test1.arn
+  versioning = {
+    enabled = true
+  }
 }
