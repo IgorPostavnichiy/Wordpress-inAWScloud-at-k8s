65f16e4e78fa4c379d7ba87188b7d4800555ac9a
IgorPostavnichiy <igorpostavnichiy@gmail.com>
2023-05-28 20:45:13 +0300

    ci/cd test3

--- terraform-aws-eks/scripts/create-s3.sh
+++ terraform-aws-eks/scripts/create-s3.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 
-bucket_name="s3-bucket-test-itsprout"
+bucket_name="your-bucket-name"
 region="eu-north-1"
 
-aws s3api head-bucket --bucket "$bucket_name"  2>/dev/null
-# aws s3api create-bucket --bucket "$bucket_name" --region eu-north-1 2>/dev/null
-
-
-if [[ $? -eq 0 ]]; then
-  echo "Bucket $bucket_name alredy exist."
-else
-  aws s3api create-bucket --bucket "$bucket_name" 
-  echo "Bucket $bucket_name created successfully."
-fi
-
-
-#!/bin/bash
-
-# bucket_name="your-bucket-name"
-# region="eu-north-1"
-
-# aws s3api create-bucket --bucket "$bucket_name" --region "$region" --create-bucket-configuration LocationConstraint="$region"
+aws s3api create-bucket --bucket "$bucket_name" --region "$region" --create-bucket-configuration LocationConstraint="$region"
