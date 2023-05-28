d4bd24d1830030cbbe1d0a05a5439e8ffd914751
IgorPostavnichiy <igorpostavnichiy@gmail.com>
2023-05-28 17:51:58 +0300

    ci/cd test

--- terraform-aws-eks/scripts/mysql.sh
+++ terraform-aws-eks/scripts/mysql.sh
@@ -4,5 +4,5 @@ kubectl exec -i service/mysql -- mysql -u root -p1234 -e "\
     CREATE DATABASE IF NOT EXISTS ddd_test; \
     USE ddd_test; \
     CREATE TABLE IF NOT EXISTS test_table (id INT PRIMARY KEY, name VARCHAR(50)); \
-    INSERT IGNORE INTO test_table (id, name) VALUES (1, 'Igor'), (2, 'TEST'), (3, 'AWS'); \
+    INSERT IGNORE INTO test_table (id, name) VALUES (1, 'Value1'), (2, 'Value2'), (3, 'Value3'); \
     Show databases;"
