7ccc24815890756c20e7c769a76b787aa281a83e
IgorPostavnichiy <igorpostavnichiy@gmail.com>
2023-05-28 20:15:52 +0300

    ci/cd test

--- terraform-aws-eks/backend.tf
+++ terraform-aws-eks/backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "s3-bucket-test-itsprout"  
-#     key            = "terraform.tfstate"   
-#     region         = "eu-north-1"           
-#     encrypt        = true                  
-#     dynamodb_table = "terraform-lock" 
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "s3-bucket-test-itsprout"  
+    key            = "terraform.tfstate"   
+    region         = "eu-north-1"           
+    encrypt        = true                  
+    dynamodb_table = "terraform-lock" 
+  }
+}
