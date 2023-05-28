f215e49433741a51d5b1fcd5ff665d2959e691b2
IgorPostavnichiy <igorpostavnichiy@gmail.com>
2023-05-28 20:43:30 +0300

    ci/cd test2

--- terraform-aws-eks/scripts/create-s3.sh
+++ terraform-aws-eks/scripts/create-s3.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 bucket_name="s3-bucket-test-itsprout"
+region="eu-north-1"
 
-# aws s3api head-bucket --bucket "$bucket_name"  2>/dev/null
-aws s3api create-bucket --bucket "$bucket_name" --region eu-north-1 2>/dev/null
+aws s3api head-bucket --bucket "$bucket_name"  2>/dev/null
+# aws s3api create-bucket --bucket "$bucket_name" --region eu-north-1 2>/dev/null
 
 
 if [[ $? -eq 0 ]]; then
@@ -12,3 +13,11 @@ else
   aws s3api create-bucket --bucket "$bucket_name" 
   echo "Bucket $bucket_name created successfully."
 fi
+
+
+#!/bin/bash
+
+# bucket_name="your-bucket-name"
+# region="eu-north-1"
+
+# aws s3api create-bucket --bucket "$bucket_name" --region "$region" --create-bucket-configuration LocationConstraint="$region"
