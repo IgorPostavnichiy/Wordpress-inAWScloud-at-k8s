44066fb1521b633f919c38389831a57be7acbe75
IgorPostavnichiy <igorpostavnichiy@gmail.com>
2023-05-28 20:46:58 +0300

    ci/cd test4

--- terraform-aws-eks/scripts/create-s3.sh
+++ terraform-aws-eks/scripts/create-s3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bucket_name="your-bucket-name"
+bucket_name="s3-bucket-test-itsprout"
 region="eu-north-1"
 
 aws s3api create-bucket --bucket "$bucket_name" --region "$region" --create-bucket-configuration LocationConstraint="$region"
